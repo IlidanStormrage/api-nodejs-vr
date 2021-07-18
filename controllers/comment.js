cd9997edea6caaa19e120e119bce9390c89d3817
IlidanStormrage <rmmartinez929294@gmail.com>
2021-07-18 12:21:24 -0500

    apirest-nodejs-user-add-edit-delete-comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -61,17 +61,98 @@ var controller = {
     });
   },
   update: function (req, res) {
-    return res.status(200).send({
-      message: "Metodo de editar comentario",
-    });
+    // CONSEGUIR DE COMENTARIO QUE LLEGA DE LA URL
+    var commentId = req.params.commentId;
+
+    // RECOGER DATOS Y VALIDAR
+    var params = req.body;
+    // VALIDAR LOS DATOS
+    try {
+      var validate_content = !validator.isEmpty(params.content);
+    } catch (err) {
+      return res.status(200).send({
+        message: "No has comentado nada",
+      });
+    }
+    if (validate_content) {
+      // FIND AND UPDATE DE SUBDOCUMENTO
+      Topic.findOneAndUpdate(
+        { "comments._id": commentId },
+        {
+          $set: { "comments.$.content": params.content },
+        },
+        { new: true },
+        (err, topicUpdated) => {
+          if (err) {
+            return res.status(500).send({
+              status: "Error",
+              message: "Error en la peticion",
+            });
+          }
+          if (!topicUpdated) {
+            return res.status(404).send({
+              status: "Error",
+              message: "No existe el tema",
+            });
+          }
+          // DEVOLVER LOS DATOS
+          return res.status(200).send({
+            status: "success",
+            topic: topicUpdated,
+          });
+        }
+      );
+    }
   },
   delete: function (req, res) {
-    return res.status(200).send({
-      message: "Metodo de borrado de comentarios",
+    // SACAR EL ID DEL TOPIC Y DEL COMENTARIO
+    var topicId = req.params.topicId;
+    var commentId = req.params.commentId;
+
+    // BUSCAR EL TOPIC
+    Topic.findById(topicId, (err, topic) => {
+      if (err) {
+        return res.status(500).send({
+          status: "Error",
+          message: "Error en la peticion",
+        });
+      }
+      if (!topic) {
+        return res.status(404).send({
+          status: "Error",
+          message: "No existe el tema",
+        });
+      }
+      //SELECCIONAR EL SUBDOCUMENTO(COMENTARIO)
+      var comment = topic.comments.id(commentId);
+
+      // BORRAR EL COMENTARIO
+      if (comment) {
+        comment.remove();
+        // GUARDAR EL TOPIC
+        topic.save(err => {
+          if (err) {
+            return res.status(500).send({
+              status: "Error",
+              message: "Error en la peticion",
+            });
+          }
+
+          //DEVOLVER  UN RESULTADO
+          return res.status(200).send({
+            status: "success",
+            topic,
+          });
+        });
+      } else {
+        return res.status(200).send({
+          message: "NO existe el comentario",
+        });
+      }
     });
   },
 };
 
-//ERROR 172 NO EXISTE EL TEMA BOTA POSTMAN
+//ERROR 174 NO EXISTE EL TEMA BOTA POSTMAN
 
 module.exports = controller;
